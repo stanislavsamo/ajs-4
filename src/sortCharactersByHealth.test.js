f62c976baf70f53a7835d4d93d38e1043932f026
stanislavsamo <f11samokhin@gmail.com>
2024-02-28 22:21:49 +0500

    add spaces

--- src/sortCharactersByHealth.test.js
+++ src/sortCharactersByHealth.test.js
@@ -1,21 +1,21 @@
 import sortCharacters from './sortCharactersByHealth.js';
 
 test.each([
-  [{
-    inputMassive: [
-      { name: 'мечник', health: 10 },
-      { name: 'маг', health: 100 },
-      { name: 'лучник', health: 80 },
-    ],
-  },
-  {
-    outputMassive: [
-      { name: 'маг', health: 100 },
-      { name: 'лучник', health: 80 },
-      { name: 'мечник', health: 10 },
-    ],
-  }],
-])('testing sortCharacters func ', ({ inputMassive, outputMassive }) => {
-  const result = sortCharacters(inputMassive);
-  expect(result).toEqual(outputMassive);
-});
+    [{
+      inputMassive: [
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+      ],
+    },
+    {
+      outputMassive: [
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+        { name: 'мечник', health: 10 },
+      ],
+    }],
+  ])('testing sortCharacters func', ({ inputMassive, outputMassive }) => {
+    const result = sortCharacters(inputMassive);
+    expect(result).toEqual(outputMassive);
+  });
